3a6486c1e0926f788385cc4e787388f83b913fec
victorvacarescu <victor.vacarescu@gmail.com>
2019-06-26 11:43:55 +0300

    26.06.2019

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>  
+        <b-nav tabs>
+            <b-nav-item><b>Inapoi</b></b-nav-item>
+            <b-nav-item disabled>Counter: 1/10</b-nav-item>
+        </b-nav>
+    </div>
+       
+    
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+        msg: String,
+    }
+}
+</script>
